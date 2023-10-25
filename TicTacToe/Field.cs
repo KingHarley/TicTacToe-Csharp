58a9efc76e8877e4e405476eb105cfc12abe076c
KingHarley <cweiss1995@gmail.com>
2023-10-25 23:15:24 +0800

    Completing the gamelogic as well as the basic rendering to the console window.

---
+++ TicTacToe/Field.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Field
+    {
+        public readonly int Row;
+        public readonly int Column;
+        public Field(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,12 +6,47 @@
         {
             Console.WriteLine("Hello, World!");
             var board = Board.CreateNewBoard();
-            var newBoard = board.MakeRandomMove(ActivePlayer.Player);
+            Render.RenderBoard(board);
+            Render.RenderSpace();
+            var gameState = board.GetBoardState();
 
-            while(true)
+            while(gameState == GameState.Ongoing)
             {
-                newBoard = newBoard.MakeRandomMove(ActivePlayer.Player);
+                board = Opponent.MakeRandomMove(board);
+                gameState = board.GetBoardState();
+                Render.RenderBoard(board);
+                Render.RenderSpace();
+
+                if (gameState != GameState.Ongoing)
+                    break;
+
+                var possibleMoves = board.GetPossibleMoves();
+                if (!possibleMoves.Any())
+                    throw new Exception($"There are no possible moves for the player and the game should have already ended");
+                var playerMove = Input.GetInputField(possibleMoves);
+                board = board.MakeMove(playerMove, FieldState.Player);
+                Render.RenderBoard(board);
+                Render.RenderSpace();
+                gameState = board.GetBoardState();
+
             }
+            FinishGame(gameState);
         }
+
+        static void FinishGame(GameState gameState)
+        {
+#pragma warning disable 8524
+            var finalWords = gameState switch
+            {
+                GameState.PlayerWon => "Congrats you've won!",
+                GameState.OpponentWon => "Opponent has won!",
+                GameState.Draw => "The game is a draw!",
+                GameState.Ongoing => throw new Exception($"The game is {gameState} and should not have ended")
+            } ;
+#pragma warning restore 8524
+
+            Console.WriteLine(finalWords);
+        }
+
     }
 }
